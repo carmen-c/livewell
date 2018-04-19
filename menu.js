b226f3bcec2974b347764bc3432ced2997c00152
carmen-c <carmen.csmwc@gmail.com>
2018-04-18 20:38:16 -0700

    can click entire dropdown menu bar

--- menu.js
+++ menu.js
@@ -1,11 +1,23 @@
 var dropdown = document.getElementById("menuDropdown");
 
+//toggle dropdown menu
 document.getElementById("menu").addEventListener("click", function() {
     
-   //toggle dropdown menu
    if (dropdown.style.display == "none") {
        dropdown.style.display = "block";
    } else {
        dropdown.style.display = "none";
    }
+});
+
+document.getElementById("mhome").addEventListener("click", function() {
+    location.href="index.html";
+});
+
+document.getElementById("mabout").addEventListener("click", function() {
+    location.href="about.html";
+});
+
+document.getElementById("mrecipe").addEventListener("click", function() {
+    location.href="recipes.html";
 });
